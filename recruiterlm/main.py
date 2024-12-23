5f4853b8e7124f76ada48034f6f14a4debce9b7b
Damian Barabonkov <damianb@alum.mit.edu>
2024-12-23 10:47:44 -0500

    refactor: Simplify user repository retrieval

--- recruiterlm/main.py
+++ recruiterlm/main.py
@@ -8,15 +8,11 @@ def main():
     # Search for users with location set to Berlin
     users = g.search_users("", location="Berlin")
 
+    breakpoint()
     # Print the login of each user found and their top 5 most recent repositories
     for user in users:
-        print(f"User: {user.login}")
-        
-        # Get the user object to access repositories
-        user_obj = g.get_user(user.login)
-        
         # Get repositories sorted by last push date
-        repos = user_obj.get_repos(sort='pushed', direction='desc')
+        repos = user.get_repos(sort='pushed', direction='desc')
         
         # Print the top 5 most recent repositories
         for i, repo in enumerate(repos[:5], start=1):
